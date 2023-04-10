5d478d32990dd1479ba83c01c29789569cedee86
Matt Schenkman <matt.schenkman@gmail.com>
2023-04-10 16:27:16 -0400

    initial script creation

---
+++ AccountExpiration.ps1
@@ -0,0 +1,16 @@
+$users = Search-ADAccount -UsersOnly -AccountExpiring -TimeSpan 31:0:0:0.0
+
+ForEach($user in $users)
+{
+    $userobj = $user | Get-ADUser -Properties EmailAddress,AccountExpirationDate
+
+    $options = @{
+        'To' = $userobj.EmailAddress
+        'From' = 'administrator@domain.org'
+        'Subject' = "Account is Expiring on $($userobj.AccountExpirationDate)"
+        'SMTPServer' = 'svr.domain.local'
+        'Body' = "Account is Expiring on $($userobj.AccountExpirationDate)"
+    }
+
+    Send-MailMessage @options
+}
